d2d2258693d9fc5ea68e7723c91946e67735a731
Fabiano Petronetto <55193884+fbipetro@users.noreply.github.com>
2019-09-12 08:43:55 -0300

    Synthetic time-varying data example
    Applying the GLoG method to define the temporal entropy diagram.

--- synthetic.py
+++ synthetic.py
@@ -21,30 +21,47 @@ patterns = tg.tegrasa(Adj=sparse.lil_matrix(A), Signal=S, binary=True)
 patterns.run()
 
 scaling = 20
-alpha = 0.7
+alpha   = 0.7
 edgecolor = 'gray'
-color_bg = 'w'
+color_bg  = 'w'
 
 G = ntx.from_numpy_matrix(A)
-slices = [5,12,30]
 
-fig1=plt.figure(1)
-fig1.set_facecolor(color_bg)
-for i in range(3):
-	ax1 = fig1.add_subplot(1,3,i+1)
-	ntx.draw_networkx_edges(G, pos=XY, edge_color=edgecolor,alpha=alpha)
-	plt.scatter(XY[:,0],XY[:,1],s=8,c=S[:,slices[i]],cmap=plt.cm.plasma)
-	plt.axis('equal')
-	plt.axis('off')
+########
+# SIGNAL
+########
+fig=plt.figure(1)
+fig.set_facecolor(color_bg)
+ntx.draw_networkx_edges(G, pos=XY, edge_color=edgecolor,alpha=alpha)
+plt.scatter(XY[:,0],XY[:,1],s=8,c=S[:,39],cmap=plt.cm.plasma)
+plt.axis('equal')
+plt.axis('off')
+plt.title('slice 39');
 
-fig2=plt.figure(2)
-fig2.set_facecolor(color_bg)
-for i in range(3):
-	ax2 = fig2.add_subplot(1,3,i+1)
-	ntx.draw_networkx_edges(G, pos=XY, edge_color=edgecolor,alpha=alpha)
-	sedges=patterns.get_edgemap(slices[i])
-	plt.scatter(XY[:,0],XY[:,1],s=10*sedges,c=sedges,cmap=plt.cm.gist_yarg)
-	plt.axis('equal')
-	plt.axis('off')
+##########################
+# EDGE NODES CONFIGURATION
+##########################
+fig=plt.figure(2)
+fig.set_facecolor(color_bg)
+ntx.draw_networkx_edges(G, pos=XY, edge_color=edgecolor, alpha=alpha)
+sedges=patterns.get_edgemap(39)
+plt.scatter(XY[:,0],XY[:,1], s=10*sedges,c=sedges, cmap=plt.cm.gist_yarg)
+plt.axis('equal')
+plt.axis('off')
+plt.title('slice 39');
+
+#################
+# ENTROPY DIAGRAM
+#################
+fig = plt.figure(3, figsize=[16,3])
+cf=patterns.get_clusters();
+y=patterns.get_entropy();
+x=range(len(y));
+plt.plot(y,c='gray');
+plt.scatter(x,y,s=scaling, c=cf, cmap='jet');
+cur_axes = fig.gca()
+cur_axes.axes.get_yaxis().set_visible(False)
+plt.xlabel('Time')
+plt.tight_layout()
 
 plt.show()
